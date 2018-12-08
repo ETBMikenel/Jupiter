5be028f8918a2efb8a255a6e83f10a4023570b7d
EvilTeddyBear.MN <Evilteddybear.mikenel@gmail.com>
2018-12-08 18:58:02 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include 'menu.inc'; 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jupiter Home</title>
+	<link rel="stylesheet" type="text/css" href="main.css"
+</head>
+<body>
+<div class="container-fluid">
+<div class="jumbotron">
+  <h1 class="display-3">Hello, world!</h1>
+  <p class="lead">Welcome to an intro draft to my latest parts management system: <em>Jupiter </em><br>
+                  We are still in the process of working through the various phases of development</p>
+  <hr class="my-2">
+  <p>We kindly ask for your patience with this project</p>
+</div>
+</div>
+</body>
+
+<?php include 'footer.inc';
+?>
